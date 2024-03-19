b7b7324922191df7f557b1ed4719d22c0061de28
alejandra <alejandra.palz@ucb.edu.bo>
2024-03-19 06:08:01 -0400

    Se añadió una prueba para evaluar un 'strike' en el juego, con ajustes en la función para gestionar este evento de forma precisa y efectiva

--- src/juego.js
+++ src/juego.js
@@ -1,32 +1,39 @@
 class Juego {
   constructor() {
-      this._rolls = new Array(21);
-      this._currentRoll = 0;
+    this._rolls = new Array(21);
+    this._currentRoll = 0;
   }
   roll(pins){
-      this._rolls[this._currentRoll++]=pins;
+    this._rolls[this._currentRoll++]=pins;
+ 
   }
   score(){
-      let score=0
-      let i=0
-      for (let frame=0; frame< 10;frame++)
+    let score=0
+    let i=0
+    for (let frame=0; frame< 10;frame++)
+    {
+      if (this._rolls[i]==10) 
       {
-          if (this.isSpare(i))
-          {
-            score=score+10+this._rolls[i+2]
-            i=i+2
-          }
-          else
-          {
-            score=score+this._rolls[i]+this._rolls[i+1]
-            i=i+2
-          }
+        score=score+10+this._rolls[i+1]+this._rolls[i+2]
+        i++
       }
-      return score
-  }
-
-  isSpare(i){
-      return this._rolls[i]+this._rolls[i+1]==10
+      else if (this.isSpare(i))
+      {
+        score=score+10+this._rolls[i+2]
+        i=i+2
+      }
+      else
+      {
+        score=score+this._rolls[i]+this._rolls[i+1]
+        i=i+2
+      }
+      
     }
+    return score
+  }
+  isSpare(i){
+    return this._rolls[i]+this._rolls[i+1]==10
+  }
+  
 }
 export default Juego;
