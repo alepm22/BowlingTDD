fc1585fda589b34fd519e8203279e2a132b997fa
alejandra <alejandra.palz@ucb.edu.bo>
2024-03-19 05:27:27 -0400

    En una serie de 20 tiros, se obtuvo cero en cada uno, dando como resultado un puntaje total de 0.

--- src/juego.js
+++ src/juego.js
@@ -1,5 +1,10 @@
 class Juego {
   constructor() {
   }
+  roll(pins){
+  }
+  score(){
+    return 0
+  }
 }
 export default Juego;
--- src/juego.test.js
+++ src/juego.test.js
@@ -5,4 +5,12 @@ describe("juego", () => {
     const myJuego = new Juego();
     expect(myJuego).toBeDefined();
   });
+  it("Debe crearse la clase juego pero vacía ", () => {
+    const g = new Juego();
+    for (let i=0;i<20;i++)
+    {
+      g.roll(0);
+    }
+    expect(g.score()).toEqual(0);
+  });
 });
