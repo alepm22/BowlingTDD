5faf3f1188d5112994ed52c0d87f693523331d0f
alejandra <alejandra.palz@ucb.edu.bo>
2024-03-19 05:55:13 -0400

    Se ha implementado código para crear una instancia fresca de la clase Game en cada prueba, asegurando un entorno de prueba independiente y libre de influencias anteriores.

--- src/juego.test.js
+++ src/juego.test.js
@@ -1,19 +1,25 @@
 import Juego from "./juego.js";
 
 describe("juego", () => {
+    let g; 
+
+  beforeEach(() => {
+    g = new Juego();
+  });
+  
   it("Debe crearse la clase juego pero vacía ", () => {
     const myJuego = new Juego();
     expect(myJuego).toBeDefined();
   });
-  const g = new Juego();
- 
+
   function rollMany(n,pins){
     for (let i=0;i<n;i++)
     {
       g.roll(pins);
     }
+
   }
-  
+
   it("Debe tirar 20 veces el jugador derribando cero pinos en cada tiro y dando score 0 ", () => {
     let n=20
     let pins=0
@@ -25,4 +31,5 @@ describe("juego", () => {
     rollMany(20,1)
     expect(g.score()).toEqual(20);
   });
+
 });
